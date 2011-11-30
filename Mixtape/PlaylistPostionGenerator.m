085b5521960e8bd87996bb5daf9454ede68e8048
orta <orta.therox@gmail.com>
2011-11-30 13:43:26 -0500

    added phone based track locations

--- Mixtape/PlaylistPostionGenerator.m
+++ Mixtape/PlaylistPostionGenerator.m
@@ -21,7 +21,7 @@
     // 5 - 250/259 - 765/259 - 508/431  - 250/618 - 759/618
     
     MixtapeAppDelegate * appDelegate = (MixtapeAppDelegate *)[[UIApplication sharedApplication] delegate];
-    NSArray *points = [NSArray array];
+    NSArray *points = nil;
     
     if ( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad ){
         
@@ -65,9 +65,51 @@
                           , nil];
                 break;    
         }
-        return points;
     }
-    return nil;
+    
+    if ( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone ){
+        switch ([appDelegate.playlists count]) {
+            case 1:
+                points = [NSArray arrayWithObject: 
+                          [[AlbumRef alloc] refWithX:246 y:175 andScale:1.0]];
+                break;
+                
+            case 2:
+                points = [NSArray arrayWithObjects: 
+                          [[AlbumRef alloc] refWithX:159 y:175 andScale:1.0],
+                          [[AlbumRef alloc] refWithX:351 y:175 andScale:1.0]
+                          , nil];
+                break;
+                
+            case 3:
+                points = [NSArray arrayWithObjects: 
+                          [[AlbumRef alloc] refWithX:254 y:210 andScale:0.9],
+                          [[AlbumRef alloc] refWithX:138 y:140 andScale:0.9],
+                          [[AlbumRef alloc] refWithX:389 y:140 andScale:0.9]
+                          , nil];
+                break;
+                
+            case 4:
+                points = [NSArray arrayWithObjects: 
+                          [[AlbumRef alloc] refWithX:140 y:135 andScale:0.8],
+                          [[AlbumRef alloc] refWithX:337 y:139 andScale:0.8],
+                          [[AlbumRef alloc] refWithX:337 y:257 andScale:0.8],
+                          [[AlbumRef alloc] refWithX:150 y:257 andScale:0.8]
+                          , nil];
+                break;
+                
+            case 5:
+                points = [NSArray arrayWithObjects: 
+                          [[AlbumRef alloc] refWithX:117 y:107 andScale:0.7],
+                          [[AlbumRef alloc] refWithX:358 y:107 andScale:0.7],
+                          [[AlbumRef alloc] refWithX:238 y:179 andScale:0.7],
+                          [[AlbumRef alloc] refWithX:117 y:257 andScale:0.7],
+                          [[AlbumRef alloc] refWithX:355 y:257 andScale:0.7]
+                          , nil];
+                break;    
+        }
+    }
+    return points;
 }
 
 @end
